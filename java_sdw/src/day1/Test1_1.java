086b91e6907ef745dba0c67319bac5befe1ac33f
systemki <wooxp@naver.com>
2021-04-26 17:37:07 +0900

    자바1일차 :  연습문제

---
+++ java_sdw/src/day1/Test1_1.java
@@ -0,0 +1,48 @@
+package day1;
+
+
+public class Test1_1 {
+
+	public static void main(String[] args) {
+		//  사과의 갯수 5개를 변수에 저장하려 한다. 이때 필요한 변수를 선언하세요.
+		
+		int aplleCount = 5;
+		
+		System.out.println(aplleCount);
+		//수학, 국어, 영어 성적을 저장하려 한다. 이때 필요한 변수를 선언하세요.
+		//단, 성적 0~100점 사이고, 소수점은 없음
+		
+		int sutdentMth;
+		int sutdentkorean;
+		int sutdentEng;
+		int sutdentAvg;
+		
+		//수학, 국어 ,영어 성적의 평균을 저장하려 한다. 이떄 필요한 변수를 선언하세요.
+		double aver;
+		
+		// 학생 이름으로 저장하기 위한 변수를 선언하세요
+		String name;
+		
+		
+		// 국어 성적을 학점을 저장하기 위한 변수를 선안 하세요
+		int sutdentKorean;
+		
+		
+		// 단 성적은 A,B,C,D,F만 사용
+		
+		char passNum;
+
+		
+		// 음악 과목  과목의 이수 여부를 확인 하기 위한 변수를 선언하세요.
+		
+		boolean  passMusic;
+		
+		
+		
+		
+		
+	
+
+	}
+
+}
