9d568cfb81fc24f21ea52b026c4bf7c8f0912b5f
systemki <wooxp@naver.com>
2021-04-27 17:50:56 +0900

    2일차

---
+++ java_sdw/src/day2/If1.java
@@ -0,0 +1,18 @@
+package day2;
+
+public class If1 {
+
+	public static void main(String[] args) {
+		// if 예제1 
+		int num = 2;
+		if(num == 0) {
+			System.out.println("0입니당");
+		} else if(num ==1 ) { 
+			System.out.println("0이 아닙니다.");
+		} else {
+			System.out.println("0과 1이 아닙니다.");
+		}
+
+	}
+
+}
