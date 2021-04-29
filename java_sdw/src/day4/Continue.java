292fceaac59c16a772fa92fa6aadad0d05c662d4
systemki <wooxp@naver.com>
2021-04-29 13:00:42 +0900

    예제 4일차 : continue

---
+++ java_sdw/src/day4/Continue.java
@@ -0,0 +1,17 @@
+package day4;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		// 
+		int i;
+		for( i = 1  ; i <=5  ; i+=1) {
+			if(i == 2) {
+				continue;
+			}
+			System.out.println(i);
+		
+		}
+	}
+
+}
